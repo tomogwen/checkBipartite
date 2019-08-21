1249c4ee1e26e281dbe5746cad8f039d59102a61
Tom Davies <tom.davies.96@hotmail.co.uk>
2019-08-21 16:57:48 +0100

    finished

--- cy.js
+++ cy.js
@@ -1,68 +1,99 @@
-var cy = cytoscape({
+var cy;
+var nodeCount;
+var edgeCount;
+var col1All;
+var col2All;
 
-  container: document.getElementById('cy'), // container to render in
+function init() {
 
-  elements: [ // list of graph elements to start with
-      /*{ // node a
-              data: { id: '1' }
-        },
-        { // node b
-              data: { id: '2' }
-        },
-        { // node b
-              data: { id: '3' }
-        },
-        { // node b
-              data: { id: '4' }
-        },
-        { // node b
-              data: { id: '5' }
-        },
+    cy = cytoscape({
 
-        { // edge ab
-              data: { id: 'a1b', source: '5', target: '1' }
-        },
-        { // edge ab
-              data: { id: 'ab', source: '1', target: '2' }
-        },
-        { // edge ab
-              data: { id: 'a2b', source: '2', target: '3' }
-        },
-        { // edge ab
-              data: { id: 'a12b', source: '3', target: '4' }
-        },
-        { // edge ab
-              data: { id: 'a21321', source: '4', target: '5' }
-        }*/
-    ],
-  style: [ // the stylesheet for the graph
-      {
-            selector: 'node',
-            style: {
-                    'background-color': '#002b80',
-                    'label': 'data(id)'
-                  }
-          },
-  
-      {
-            selector: 'edge',
-            style: {
-                    'width': 3,
-                    'line-color': '#6699ff',
-                    'target-arrow-color': '#6699ff',
-                    'target-arrow-shape': 'triangle'
-                  }
-          }
-    ],
-  layout: {
-      name: 'grid',
-      rows: 1
-    }
-});
+      container: document.getElementById('cy'), // container to render in
 
+      elements: [ // list of graph elements to start with
+          /*{ // node a
+                  data: { id: '1' }
+            },
+            { // node b
+                  data: { id: '2' }
+            },
+            { // node b
+                  data: { id: '3' }
+            },
+            { // node b
+                  data: { id: '4' }
+            },
+            { // node b
+                  data: { id: '5' }
+            },
 
-var nodeCount = 0;
-var edgeCount = 0;
+            { // edge ab
+                  data: { id: 'a1b', source: '5', target: '1' }
+            },
+            { // edge ab
+                  data: { id: 'ab', source: '1', target: '2' }
+            },
+            { // edge ab
+                  data: { id: 'a2b', source: '2', target: '3' }
+            },
+            { // edge ab
+                  data: { id: 'a12b', source: '3', target: '4' }
+            },
+            { // edge ab
+                  data: { id: 'a21321', source: '4', target: '5' }
+            } */
+        ],
+      style: [ // the stylesheet for the graph
+          {
+                selector: 'node',
+                style: {
+                        'background-color': '#002b80',
+                        //'label': 'data(id)'
+                      }
+              },
+      
+          {
+                selector: 'edge',
+                style: {
+                        'width': 3,
+                        'line-color': '#6699ff',
+                        'target-arrow-color': '#6699ff',
+                        'target-arrow-shape': 'triangle'
+                      }
+              }
+        ],
+      layout: {
+          name: 'grid',
+          rows: 1
+        }
+    });
+
+    /*let clicks = [];
+    //let nodes = [false, false, false, false];
+    let nodeclicks = 0;
+    let held;
+    cy.on('click', 'node', function(evt){
+        if (held !== true)  {
+            clicks.push(evt.target.id());
+            edgeCount++;
+            if (nodeclicks > 0) {
+                cy.add({data: {id: -edgeCount, source: clicks[nodeclicks-1], target: clicks[nodeclicks], selector: edgeCount}});
+            }   
+            nodeclicks ++;
+            if (nodeclicks == 2)  {
+                clicks = [];
+                nodeclicks =0;
+            }
+        } else {
+            held = false;
+        }
+    });
+    */
+
+    var sortBut = document.getElementById('dispBut');
+    sortBut.style.display = 'none';
+
+}
 
 /*cy.on("tap", function(e) {
 
@@ -76,6 +107,7 @@ var edgeCount = 0;
     })
     nodeCount += 1;
 });*/
+
 
 
 function runCose() {
@@ -103,12 +135,12 @@ function binomial(n, k) {
 }
 
 
-function randomEdges() {
-    for (var i = 0; i< Math.floor(1*(nodeCount-1)); i++) {
-        var startId = Math.floor(Math.random() * nodeCount); 
+function randomEdges(numNodes) {
+    for (var i = 0; i< Math.floor(1*(numNodes-1)); i++) {
+        var startId = Math.floor(Math.random() * numNodes); 
         var endId = startId;
         while (endId === startId) {
-            endId   = Math.floor(Math.random() * nodeCount);
+            endId   = Math.floor(Math.random() * numNodes);
         }
         
         cy.add({
@@ -124,11 +156,12 @@ function randomEdges() {
 }
 
 
-function randomNodes() { 
-    for (var i = 0; i<10; i++) {
+function randomNodes(numNodes) { 
+    for (var i = 0; i<numNodes; i++) {
         cy.add({
             group: 'nodes',
-            data: { id: nodeCount},
+            //data: { id: nodeCount},
+            data: {id: i},
             position: {
                 x: Math.floor(Math.random() * screen.height),
                 y: Math.floor(Math.random() * screen.width)
@@ -149,8 +182,8 @@ function deleteIsolated() {
 
 
 function tryColour(initKey) {      
-    var col1All = cy.collection();
-    var col2All = cy.collection();
+    col1All = cy.collection();
+    col2All = cy.collection();
     var col1 = [];
     var col2 = [];
 
@@ -307,47 +340,70 @@ function checkBipartite() {
     // tryColour ONLY WORKS ON CONNECTED GRAPHS
     var result = tryColour( key );    
     console.log(result);
+
+    if(result) {
+        var sortBut = document.getElementById('dispBut');
+        sortBut.style.display = 'block';
+    }
+
 }
 
 
+function deleteComponents() {
+    var col  = cy.elements();
+    var comp = col.components();
 
-// Joes Code
-let clicks = [];
-//let nodes = [false, false, false, false];
-let nodeclicks = 0;
-let held;
-cy.on('click', 'node', function(evt){
-    if (held !== true)  {
-        clicks.push(evt.target.id());
-        edgeCount++;
-        if (nodeclicks > 0) {
-            cy.add({data: {id: -edgeCount, source: clicks[nodeclicks-1], target: clicks[nodeclicks], selector: edgeCount}});
-        }   
-        nodeclicks ++;
-        if (nodeclicks == 2)  {
-            clicks = [];
-            nodeclicks =0;
+    var bestLen = 0;
+    var bestInd = 0;
+    for(var i=0; i<comp.length; i++){
+        if(comp.length > bestLen) { 
+            bestLen = comp.length;
+            bestInd = i;
         }
-    } else {
-        held = false;
     }
-});
-/*
-cy.on('taphold', 'node', function(evt) {
-    held = true;
-    let nod = evt.target.id();
-    let ind = chrs.indexOf(nod);
-    chrs.splice(ind, 1);
-    console.log(chrs);
-    cy.remove(cy.$id(nod));
-});
-cy.on('click', 'edge', function(evt){
-    let ed = evt.target.id();
-    cy.remove(cy.$id(ed));
-});*/
+    for(var i=0; i<comp.length; i++){
+        if( i!== bestInd) {
+            cy.remove(comp[i]);
+        }
+    }
+}
 
 
+function genGraph() {
+
+    var nodeCount = 0;
+    var edgeCount = 0;
+    var numNodes = 10;
+    
+    init();
+    console.clear();
+    randomNodes(numNodes);
+    randomEdges(numNodes);
+    deleteIsolated();
+    deleteComponents();
+    
+    var options = {
+        name: 'cose',
+        animate: 'false'
+    };
+    var layout = cy.layout(options);
+    layout.run();
+
+}
 
 
+function moveToColours() {
+
+    for(var i=0; i<col1All.length; i++) {
+        col1All[i].position('x','10');
+        col1All[i].position('y',10+i*60);
+    }   
+
+    for(var i=0; i<col2All.length; i++) {
+        col2All[i].position('x', '100');
+        col2All[i].position('y', 10+i*60);
+    }
+    cy.fit(col1All.union(col2All), 100);
+}
 
 
